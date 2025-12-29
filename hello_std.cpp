c0374d291293cfa2e6e3ee517bedf3739c3791fd
Constantin Câmpean <mail.la.costi@gmail.com>
2025-12-29 22:11:38 +0200

    std namespace

---
+++ hello_std.cpp
@@ -0,0 +1,10 @@
+#include <iostream>  // Include the iostream library for input/output
+using namespace std; // Bring the std namespace into the current scope
+
+int main()
+{
+    // Print "Hello, World!" to the console
+    cout << "Hello, World!" << endl;
+    // Return 0 to indicate successful execution
+    return 0;
+}
